6b7d4e8406c7d207d7305a572dd60d37b34b2efc
Hojun Kim <hojunkim2@gmail.com>
2022-11-30 20:17:32 -0800

    created beatmaker project and html skeleton

---
+++ app.js
@@ -0,0 +1,42 @@
+// Imports
+import { sounds } from "./data.js";
+
+// Query Selectors
+const clapSelect = document.querySelector("#clap-select");
+const hihatSelect = document.querySelector("#hihat-select");
+const kickSelect = document.querySelector("#kick-select");
+const percSelect = document.querySelector("#perc-select");
+const snareSelect = document.querySelector("#snare-select");
+const tomSelect = document.querySelector("#tom-select");
+const clapPad = document.querySelector(".clap");
+const hihatPad = document.querySelector(".hihat");
+const kickPad = document.querySelector(".kick");
+const percPad = document.querySelector(".perc");
+const snarePad = document.querySelector(".snare");
+const tomPad = document.querySelector(".tom");
+
+const types = [
+   [clapSelect, clapPad, "clap"],
+   [hihatSelect, hihatPad, "hihat"],
+   [kickSelect, kickPad, "kick"],
+   [percSelect, percPad, "perc"],
+   [snareSelect, snarePad, "snare"],
+   [tomSelect, tomPad, "tom"],
+];
+
+for (let i = 0; i < types.length; i++) {
+   for (let j = 0; j < sounds[i].length; j++) {
+      let option = document.createElement("option");
+      option.innerText = sounds[i][j].name;
+      option.value = sounds[i][j].path;
+      types[i][0].appendChild(option);
+   }
+}
+
+for (let i = 0; i < types.length; i++) {
+   for (let j = 0; j < 8; j++) {
+      let pad = document.createElement("div");
+      pad.classList.add("pad", `${types[i][2]}-pad`, `b${j}`);
+      types[i][1].appendChild(pad);
+   }
+}
---
+++ data.js
@@ -0,0 +1,350 @@
+export const sounds = [
+   [
+      {
+         name: "808 Clap",
+         path: "sounds/clap-808.wav",
+      },
+      {
+         name: "Analog Clap",
+         path: "sounds/clap-analog.wav",
+      },
+      {
+         name: "Crushed Clap",
+         path: "sounds/clap-crushed.wav",
+      },
+      {
+         name: "Fat Clap",
+         path: "sounds/clap-fat.wav",
+      },
+      {
+         name: "Slapper Clap",
+         path: "sounds/clap-slapper.wav",
+      },
+      {
+         name: "Tape Clap",
+         path: "sounds/clap-tape.wav",
+      },
+   ],
+   [
+      {
+         name: "808 Hihat",
+         path: "sounds/hihat-808.wav",
+      },
+      {
+         name: "Acoustic 01 Hihat",
+         path: "sounds/hihat-acoustic01.wav",
+      },
+      {
+         name: "Acoustic 02 Hihat",
+         path: "sounds/hihat-acoustic02.wav",
+      },
+      {
+         name: "Analog Hihat",
+         path: "sounds/hihat-analog.wav",
+      },
+      {
+         name: "Digital Hihat",
+         path: "sounds/hihat-digital.wav",
+      },
+      {
+         name: "Dist 01 Hihat",
+         path: "sounds/hihat-dist01.wav",
+      },
+      {
+         name: "Dist 02 Hihat",
+         path: "sounds/hihat-dist02.wav",
+      },
+      {
+         name: "Electro Hihat",
+         path: "sounds/hihat-electro.wav",
+      },
+      {
+         name: "Plain Hihat",
+         path: "sounds/hihat-plain.wav",
+      },
+      {
+         name: "Reso Hihat",
+         path: "sounds/hihat-reso.wav",
+      },
+      {
+         name: "Ring Hihat",
+         path: "sounds/hihat-ring.wav",
+      },
+   ],
+   [
+      {
+         name: "808 Kick",
+         path: "sounds/kick-808.wav",
+      },
+      {
+         name: "Acoustic 01 Kick",
+         path: "sounds/kick-acoustic01.wav",
+      },
+      {
+         name: "Acoustic 02 Kick",
+         path: "sounds/kick-acoustic02.wav",
+      },
+      {
+         name: "Big Kick",
+         path: "sounds/kick-big.wav",
+      },
+      {
+         name: "Classic Kick",
+         path: "sounds/kick-classic.wav",
+      },
+      {
+         name: "Cultivator Kick",
+         path: "sounds/kick-cultivator.wav",
+      },
+      {
+         name: "Deep Kick",
+         path: "sounds/kick-deep.wav",
+      },
+      {
+         name: "Dry Kick",
+         path: "sounds/kick-dry.wav",
+      },
+      {
+         name: "Electro 01 Kick",
+         path: "sounds/kick-electro01.wav",
+      },
+      {
+         name: "Electro 02 Kick",
+         path: "sounds/kick-electro02.wav",
+      },
+      {
+         name: "Floppy Kick",
+         path: "sounds/kick-floppy.wav",
+      },
+      {
+         name: "Gritty Kick",
+         path: "sounds/kick-gritty.wav",
+      },
+      {
+         name: "Heavy Kick",
+         path: "sounds/kick-heavy.wav",
+      },
+      {
+         name: "New Wave Kick",
+         path: "sounds/kick-newwave.wav",
+      },
+      {
+         name: "Old School Kick",
+         path: "sounds/kick-oldschool.wav",
+      },
+      {
+         name: "Plain Kick",
+         path: "sounds/kick-plain.wav",
+      },
+      {
+         name: "Slapback Kick",
+         path: "sounds/kick-slapback.wav",
+      },
+      {
+         name: "Softy Kick",
+         path: "sounds/kick-softy.wav",
+      },
+      {
+         name: "Stomp Kick",
+         path: "sounds/kick-stomp.wav",
+      },
+      {
+         name: "Tape Kick",
+         path: "sounds/kick-tape.wav",
+      },
+      {
+         name: "Thump Kick",
+         path: "sounds/kick-thump.wav",
+      },
+      {
+         name: "Tight Kick",
+         path: "sounds/kick-tight.wav",
+      },
+      {
+         name: "Tron Kick",
+         path: "sounds/kick-tron.wav",
+      },
+      {
+         name: "Vinyl 01 Kick",
+         path: "sounds/kick-vinyl01.wav",
+      },
+      {
+         name: "Vinyl 02 Kick",
+         path: "sounds/kick-vinyl02.wav",
+      },
+      {
+         name: "Zapper Kick",
+         path: "sounds/kick-zapper.wav",
+      },
+   ],
+   [
+      {
+         name: "808 Perc",
+         path: "sounds/perc-808.wav",
+      },
+      {
+         name: "Chirpy Perc",
+         path: "sounds/perc-chirpy.wav",
+      },
+      {
+         name: "Hollow Perc",
+         path: "sounds/perc-hollow.wav",
+      },
+      {
+         name: "Laser Perc",
+         path: "sounds/perc-laser.wav",
+      },
+      {
+         name: "Metal Perc",
+         path: "sounds/perc-metal.wav",
+      },
+      {
+         name: "Nasty Perc",
+         path: "sounds/perc-nasty.wav",
+      },
+      {
+         name: "Short Perc",
+         path: "sounds/perc-short.wav",
+      },
+      {
+         name: "Tambo Perc",
+         path: "sounds/perc-tambo.wav",
+      },
+      {
+         name: "Tribal Perc",
+         path: "sounds/perc-tribal.wav",
+      },
+      {
+         name: "Weirdo Perc",
+         path: "sounds/perc-weirdo.wav",
+      },
+   ],
+   [
+      {
+         name: "808 Snare",
+         path: "sounds/snare-808.wav",
+      },
+      {
+         name: "Acoustic 01 Snare",
+         path: "sounds/snare-acoustic01.wav",
+      },
+      {
+         name: "Acoustic 02 Snare",
+         path: "sounds/snare-acoustic02.wav",
+      },
+      {
+         name: "Analog Snare",
+         path: "sounds/snare-analog.wav",
+      },
+      {
+         name: "Big Snare",
+         path: "sounds/snare-big.wav",
+      },
+      {
+         name: "Block Snare",
+         path: "sounds/snare-block.wav",
+      },
+      {
+         name: "Brute Snare",
+         path: "sounds/snare-brute.wav",
+      },
+      {
+         name: "Dist 01 Snare",
+         path: "sounds/snare-dist01.wav",
+      },
+      {
+         name: "Dist 02 Snare",
+         path: "sounds/snare-dist02.wav",
+      },
+      {
+         name: "Dist 03 Snare",
+         path: "sounds/snare-dist03.wav",
+      },
+      {
+         name: "Electro Snare",
+         path: "sounds/snare-electro.wav",
+      },
+      {
+         name: "Lofi 01 Snare",
+         path: "sounds/snare-lofi01.wav",
+      },
+      {
+         name: "Lofi 02 Snare",
+         path: "sounds/snare-lofi02.wav",
+      },
+      {
+         name: "Modular Snare",
+         path: "sounds/snare-modular.wav",
+      },
+      {
+         name: "Noise Snare",
+         path: "sounds/snare-noise.wav",
+      },
+      {
+         name: "Pinch Snare",
+         path: "sounds/snare-pinch.wav",
+      },
+      {
+         name: "Punch Snare",
+         path: "sounds/snare-punch.wav",
+      },
+      {
+         name: "Smasher Snare",
+         path: "sounds/snare-smasher.wav",
+      },
+      {
+         name: "Sumo Snare",
+         path: "sounds/snare-sumo.wav",
+      },
+      {
+         name: "Tape Snare",
+         path: "sounds/snare-tape.wav",
+      },
+      {
+         name: "Vinyl 01 Snare",
+         path: "sounds/snare-vinyl01.wav",
+      },
+      {
+         name: "Vinyl 02 Snare",
+         path: "sounds/snare-vinyl02.wav",
+      },
+   ],
+   [
+      {
+         name: "808 Tom",
+         path: "sounds\tom-808.wav",
+      },
+      {
+         name: "Acoustic 01 Tom",
+         path: "sounds\tom-acoustic01.wav",
+      },
+      {
+         name: "Acoustic 02 Tom",
+         path: "sounds\tom-acoustic02.wav",
+      },
+      {
+         name: "Analog Tom",
+         path: "sounds\tom-analog.wav",
+      },
+      {
+         name: "Chiptune Tom",
+         path: "sounds\tom-chiptune.wav",
+      },
+      {
+         name: "FM Tom",
+         path: "sounds\tom-fm.wav",
+      },
+      {
+         name: "Lofi Tom",
+         path: "sounds\tom-lofi.wav",
+      },
+      {
+         name: "Rotot Tom",
+         path: "sounds\tom-rototom.wav",
+      },
+      {
+         name: "Short Tom",
+         path: "sounds\tom-short.wav",
+      },
+   ],
+];
